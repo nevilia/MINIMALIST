c40f80b32021b2b820376862b84528afc19b6924
nevilia <winter04nevilia@gmail.com>
2024-02-05 16:35:48 +0530

    TRIAL REGISTER

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Home from './components/Home/Home';
 import Products from './components/Products/Products';
+import Register from './components/Authentication/Register';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
       <Footer />
     </Router>
---
+++ frontend/src/components/Authentication/Register.tsx
@@ -0,0 +1,11 @@
+function Register() {
+  return (
+    <div>
+        <h1 className="text-9xl">
+            REGISTER
+        </h1>
+    </div>
+  )
+}
+
+export default Register
