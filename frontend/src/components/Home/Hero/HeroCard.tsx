7776729af10e287e6a132e79f733e85408560a58
nevilia <winter04nevilia@gmail.com>
2024-02-06 20:34:26 +0530

    added products link

--- frontend/src/components/Home/Hero/HeroCard.tsx
+++ frontend/src/components/Home/Hero/HeroCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 function HeroCard() {
     return (
         <div className="p-6 ">
@@ -6,9 +7,11 @@ function HeroCard() {
             <p className="text-[25px] mb-10">
                 Introducing Our Exclusive Eyecare Range, with Eye Cream: Formulated to reduce Dark Circles, Puffiness & Fine Lines.
             </p>
-            <button className="bg-black text-white text-[18px] font-semibold px-[70px] py-[20px]">
-                Buy Now
-            </button>
+            <Link to="/products">
+                <button className="bg-black text-white text-[18px] font-semibold px-[70px] py-[20px]">
+                    Explore Products
+                </button>
+            </Link>
         </div>
 
     )
