8f91a0eefb7e26418d301719ce17391a14ac5fb0
nevilia <winter04nevilia@gmail.com>
2024-02-15 16:11:23 +0530

    Rating Stars added

--- frontend/src/components/Products/Product.tsx
+++ frontend/src/components/Products/Product.tsx
@@ -1,23 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-// import { Props } from './ProductCard';
-
-// let product = {
-//     "_id": "65be2dc4024a7ce5a9ded68b",
-//     "name": "Alpha Arbutin 02%",
-//     "rating": 4,
-//     "price": 549,
-//     "reviews": 1538,
-//     "type": "skin",
-//     "images": [
-//     "https://beminimalist.co/cdn/shop/products/AlphaArbutin2_1200-1-min_900x.png?v=1646543550",
-//     "https://beminimalist.co/cdn/shop/products/arbutin-2-serum-1200--2min-1645936951710_900x.webp?v=1651474651"
-//     ],
-//     "coverPhoto": "https://beminimalist.co/cdn/shop/products/AlphaArbutin2_1200-1-min_900x.png?v=1646543550",
-//     "createdAt": "2024-02-03T12:12:52.481Z",
-//     "updatedAt": "2024-02-03T12:12:52.481Z",
-//     "__v": 0
-//     }
+import Rating from '../Rating';
 
 function Product() {
   const { _id } = useParams();
@@ -39,13 +22,14 @@ function Product() {
   }
 
   return (
-    <div className='flex flex-wrap m-7 p-10 '>
+    <div className='flex flex-wrap sm:m-5 p-8 '>
       <div className='p-5  sm:w-full md:w-1/2'>
         <img className='' src={product.coverPhoto} alt={product.name} />
       </div>
       <div className='p-5 md:w-1/2'>
-        <h2 className='text-4xl font-bold pb-8'>{product.name}</h2>
-        <p className='pb-10 font-semibold'>Rating: {product.rating} Reviews: {product.reviews}</p>
+        <h2 className='text-4xl font-bold pb-8'>{product.name} </h2>
+        <Rating rating={product.rating} reviews={product.reviews} />
+
         
         <h4 className='font-semibold text-gray-800 text-xl pb-10'> Tag Line </h4>
         <p className='pb-10'>
